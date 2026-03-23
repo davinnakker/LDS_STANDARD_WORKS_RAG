3588768dbadf2a694eac2dee453bf382f9c66619
Davin van den Akker <davinnakker@yahoo.com>
2026-03-23 11:46:43 -0600

    update requirements

--- make_embeddings.py
+++
@@ -1,8 +0,0 @@
-from semantic.retrieval import Retrieval
-from app.helper_functions import get_csv
-
-works = ['Old Testament', 'New Testament', 'Book of Mormon', 'Doctrine and Covenants', 'Pearl of Great Price']
-
-for work in works:
-    csv, embeddings = get_csv(work)
-    retriever = Retrieval(csv, embeddings)
